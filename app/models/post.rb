c02560c8ba5050fe9cadab4aad9c24db9e1f67f4
B1nj0y <v@joyv.net>
2017-08-10 23:35:44 +0800

    edit Gemfile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text(65535)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
   belongs_to :user
 
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text(65535)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
